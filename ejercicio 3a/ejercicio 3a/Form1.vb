dca10f1b38ad12b4ea3c704a38d3f6481b80d04f
Lucas-sarto <rulo.sarto@gmail.com>
2023-06-08 21:23:15 -0300

    segundo commit

--- ejercicio 3a/ejercicio 3a/Form1.vb
+++ ejercicio 3a/ejercicio 3a/Form1.vb
@@ -3,4 +3,13 @@
     Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblMenor.Click
 
     End Sub
+
+    Private Sub cmdEjecutar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdEjecutar.Click
+        Dim a As Integer
+        Dim b As Integer
+        Dim suma As Integer
+        Dim mayor As Integer
+        Dim menor As Integer
+
+    End Sub
 End Class
